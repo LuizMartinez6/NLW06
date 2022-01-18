51f93f054b59335842a999eb06167f813915d2c2
LuizMartinez6 <luizmar795@gmail.com>
2022-01-18 17:47:25 -0300

    Fix: add a parentesis in server.ts line 9

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,6 @@ const createUserController = new CreateUserController()
 
 
 router.post("/users", createUserController.handle)
-router.get('/users', (req: Request, res: Response) => res.send({ message: 'hellolasodsaoasdfo'}))
 
 
 export { router }
